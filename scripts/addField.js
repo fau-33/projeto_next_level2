95e2dfe0bd17861fd267c24c9ea536c25a01e483
Flávio Félix <fau_felix30@hotmail.com>
2020-08-05 18:02:09 -0300

    Complete a function page give

---
+++ scripts/addField.js
@@ -0,0 +1,28 @@
+//Procura o botão
+document.querySelector('#add-time')
+
+//Quando clicar no botão
+.addEventListener('click', cloneField)
+
+
+
+//Executar uma ação
+function cloneField(){
+  
+
+  //Duplicar os campos.Que campos?
+  const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)//boolean: true ou false
+
+  //Pega os campos. Que campos?
+  const fields = newFieldContainer.querySelectorAll('input');
+  
+  //Para cada campo, limpar
+  fields.forEach(function(field){
+    //pega o field do momento e limpa ele
+    field.value = "";
+  })
+
+  //Colocar a página: Onde?
+  document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
+
